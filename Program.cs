49da844d455f3b4e1ead363015dd04b01951dbfc
Grace <123522499+gmc9193@users.noreply.github.com>
2023-09-12 13:27:44 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿namespace CSVLab
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //Populating list
+            var lines = File.ReadLines("videogames.csv");
+            List<Videogame> gameList = lines.Skip(1).Select(line => new Videogame(line)).ToList();
+            
+            //Test Sorting
+            Console.WriteLine("Unsorted List");
+            gameList.ForEach(Console.WriteLine);
+            gameList.Sort();
+            Console.WriteLine("Sorted List");
+            gameList.ForEach(Console.WriteLine);
+            
+            //Get Nintendo Games
+            var nintendoGames = gameList.Where(x => x.Publisher == "Nintendo").ToList();
+            nintendoGames.Sort();
+            nintendoGames.ForEach(Console.WriteLine);
+
+            // Get nintendo percentages
+            double percentage;
+            var rolePlayingGames = gameList.Where(x => x.Genre == "Role-Playing").ToList();
+
+            percentage = Math.Round(rolePlayingGames.Count * 100.0 / gameList.Count, 2);
+            Console.WriteLine($"Out of {gameList.Count}, {rolePlayingGames.Count} are Role-Playing games. Which is {percentage:F2}%.");
+            
+            percentage = Math.Round(nintendoGames.Count * 100.0 / gameList.Count, 2);
+            Console.WriteLine($"Out of {gameList.Count}, {nintendoGames.Count} are developed by Nintendo. Which is {percentage:F2}%.");
+
+            //Test Methods
+            PublisherData("Electronic Arts", gameList);
+            GenreData("Shooter", gameList);
+            
+            //User Input 
+            Console.WriteLine("Enter Publisher:");
+            PublisherData(Console.ReadLine()!, gameList);
+            
+            Console.WriteLine("Enter Genre:");
+            GenreData(Console.ReadLine()!, gameList);
+            
+        }
+
+        public static void PublisherData(string input, List<Videogame> gameList)
+        {
+            var publisherGames = gameList.Where(x => x.Publisher == input).ToList();
+            publisherGames.Sort();
+            publisherGames.ForEach(Console.WriteLine);
+
+            double percentage = (double)publisherGames.Count / gameList.Count * 100.0;
+            percentage = Math.Round(percentage, 2);
+            Console.WriteLine($"Out of {gameList.Count}, {publisherGames.Count} are developed by {input}. Which is {percentage:F2}%.");
+        }
+
+        public static void GenreData(string input, List<Videogame> gameList)
+        {
+            var genreGames = gameList.Where(x => x.Genre == input).ToList();
+            genreGames.Sort();
+            genreGames.ForEach(Console.WriteLine);
+
+            double percentage = (double)genreGames.Count / gameList.Count * 100.0;
+            percentage = Math.Round(percentage, 2);
+            Console.WriteLine($"Out of {gameList.Count}, {genreGames.Count} are {input} games. Which is {percentage:F2}%.");
+        }
+
+    }
+
+}
+
---
+++ Videogame.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+using System.Xml.Linq;
+
+namespace CSVLab;
+
+public class Videogame : IComparable
+{
+    public string Name { get; set; }
+    public string Platform { get; set; }
+    public DateOnly Year { get; set; }
+    public string Genre { get; set; }
+    public string Publisher { get; set; }
+    public float NaSales { get; set; }
+    public float EuSales { get; set; }
+    public float JpSales { get; set; }
+    public float OtherSales { get; set; }
+    public float GlobalSales { get; set; }
+
+    public Videogame(string csvInput)
+    {
+        var elements = csvInput.Split(',');
+        Name = elements[0];
+        Platform = elements[1];
+        Year = DateOnly.Parse("01/01/" + elements[2]);
+        Genre = elements[3];
+        Publisher = elements[4];
+        NaSales = float.Parse(elements[5]);
+        EuSales = float.Parse(elements[6]);
+        JpSales = float.Parse(elements[7]);
+        OtherSales = float.Parse(elements[8]);
+        GlobalSales = float.Parse(elements[9]);
+    }
+
+
+public override string ToString()
+{
+    return $"{nameof(Name)}: {Name}, " +
+           $"{nameof(Platform)}: {Platform}, " +
+           $"{nameof(Year)}: {Year}, " +
+           $"{nameof(Genre)}: {Genre}, " +
+           $"{nameof(Publisher)}: {Publisher}, " +
+           $"{nameof(NaSales)}: {NaSales.ToString("F2", CultureInfo.InvariantCulture)}, " +
+           $"{nameof(EuSales)}: {EuSales.ToString("F2", CultureInfo.InvariantCulture)}, " +
+           $"{nameof(JpSales)}: {JpSales.ToString("F2", CultureInfo.InvariantCulture)}, " +
+           $"{nameof(OtherSales)}: {OtherSales.ToString("F2", CultureInfo.InvariantCulture)}, " +
+           $"{nameof(GlobalSales)}: {GlobalSales.ToString("F2", CultureInfo.InvariantCulture)}";
+}
+    public int CompareTo(object? obj)
+    {
+        if (obj is Videogame vid)
+        {
+            return string.Compare(this.Name, vid.Name, StringComparison.InvariantCultureIgnoreCase);
+        }
+        return -1;
+    }
+}
